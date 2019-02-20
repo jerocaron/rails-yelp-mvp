bcfd8ede396fcbd60cc367f93c40f13c8f71b3b5
Jerome Caron <jerocaron@gmail.com>
2019-02-20 17:35:23 +0100

    Resto MVP to be completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '+44 099 456 234',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '+44 099 456 234',
+    category:     'italian'
+  },
+  {
+    name:         'Mamamia',
+    address:      '7 Boundary St, Cambridge E2 7JE',
+    phone_number: '+44 099 456 234',
+    category:     'italian'
+  },
+  {
+    name:         'Le bistronomique',
+    address:      '56, avenue Foch, Paris 75000',
+    phone_number: '+44 099 456 234',
+    category:     'french'
+  },
+  {
+    name:         'Chez mon boucher',
+    address:      '56, avenue Lange, Liège B53267',
+    phone_number: '+44 099 456 234',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
